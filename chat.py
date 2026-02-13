a44b4d69cdffc870c6b49c601f7bf3eae692fa98
peichen.tsai <nazonerd@icloud.com>
2026-02-13 15:13:45 +0800

    new chatbot for new tensorflow

---
+++ chat.py
@@ -0,0 +1,68 @@
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+
+lemmatizer = WordNetLemmatizer()
+
+# 1. Load the data and the model
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.h5')
+
+def clean_up_sentence(sentence):
+    # Tokenize and lemmatize user input
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bow(sentence, words, show_details=True):
+    # Create the Bag of Words (0s and 1s) for the user's sentence
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence, model):
+    # Filter out predictions below a threshold
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+
+    # Sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def get_response(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+print("GO! Bot is running! (Type 'quit' to exit)")
+
+while True:
+    message = input("")
+    if message.lower() == 'quit':
+        break
+
+    ints = predict_class(message, model)
+    if len(ints) > 0:
+        res = get_response(ints, intents)
+        print(f"Bot: {res}")
+    else:
+        print("Bot: I'm sorry, I don't quite understand that.")
--- train_chatbot.py
+++ train_chatbot.py
@@ -1,98 +1,110 @@
-import nltk
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
 import json
 import pickle
-
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-#from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.optimizers.legacy import SGD
-import random
+import nltk
+from nltk.stem import WordNetLemmatizer
 
-words=[]
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
+
+# Initialize Lemmatizer
+lemmatizer = WordNetLemmatizer()
+
+# --- 1. DATA PREPROCESSING ---
+words = []
 classes = []
 documents = []
-ignore_words = ['?', '!']
+ignore_words = ['?', '!', '.', ',']
+
+# Load the intents file
 data_file = open('intents.json').read()
 intents = json.loads(data_file)
 
-
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-
-        #tokenize each word
+        # Tokenize each word
         w = nltk.word_tokenize(pattern)
         words.extend(w)
-        #add documents in the corpus
+        # Add to documents
         documents.append((w, intent['tag']))
-
-        # add to our classes list
+        # Add to classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-# lemmaztize and lower each word and remove duplicates
+# Lemmatize and lower each word and remove duplicates
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
-# sort classes
 classes = sorted(list(set(classes)))
-# documents = combination between patterns and intents
-print (len(documents), "documents")
-# classes = intents
-print (len(classes), "classes", classes)
-# words = all words, vocabulary
-print (len(words), "unique lemmatized words", words)
 
+print(f"{len(documents)} documents")
+print(f"{len(classes)} classes: {classes}")
+print(f"{len(words)} unique lemmatized words")
 
-pickle.dump(words,open('words.pkl','wb'))
-pickle.dump(classes,open('classes.pkl','wb'))
+# Save words and classes for the chat script to use later
+pickle.dump(words, open('words.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
-# create our training data
-training = []
-# create an empty array for our output
+# --- 2. CREATE TRAINING DATA ---
+train_x = []
+train_y = []
+# Create an empty array for our output
 output_empty = [0] * len(classes)
-# training set, bag of words for each sentence
+
 for doc in documents:
-    # initialize our bag of words
+    # Bag of words
     bag = []
-    # list of tokenized words for the pattern
+    # List of tokenized words for the pattern
     pattern_words = doc[0]
-    # lemmatize each word - create base word, in attempt to represent related words
+    # Lemmatize each word - create base word, in attempt to represent related words
     pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words]
-    # create our bag of words array with 1, if word match found in current pattern
+    
+    # Create our bag of words array with 1, if word match found in current pattern
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
-    
-    # output is a '0' for each tag and '1' for current tag (for each pattern)
+
+    # Output is a '0' for each tag and '1' for current tag (one-hot encoding)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
-    
-    training.append([bag, output_row])
-# shuffle our features and turn into np.array
-random.shuffle(training)
-training = np.array(training)
-# create train and test lists. X - patterns, Y - intents
-train_x = list(training[:,0])
-train_y = list(training[:,1])
-print("Training data created")
 
+    train_x.append(bag)
+    train_y.append(output_row)
 
-# Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
-# equal to number of intents to predict output intent with softmax
-model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(len(train_y[0]), activation='softmax'))
+# Convert to NumPy arrays separately (Fixes the ValueError)
+train_x = np.array(train_x)
+train_y = np.array(train_y)
 
-# Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+# --- 3. DEFINE MODERN OPTIMIZER (Exponential Decay) ---
+lr_schedule = ExponentialDecay(
+    initial_learning_rate=0.01,
+    decay_steps=10000, 
+    decay_rate=0.96,
+    staircase=True
+)
 
-#fitting and saving the model 
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+optimizer = SGD(
+    learning_rate=lr_schedule, 
+    momentum=0.9, 
+    nesterov=True
+)
+
+# --- 4. BUILD THE MODEL ---
+model = Sequential([
+    Dense(128, input_shape=(len(train_x[0]),), activation='relu'),
+    Dropout(0.5),
+    Dense(64, activation='relu'),
+    Dropout(0.5),
+    Dense(len(train_y[0]), activation='softmax')
+])
+
+model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+
+# --- 5. TRAINING ---
+# We use fit directly on train_x and train_y
+hist = model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+
+# Save model
 model.save('chatbot_model.h5', hist)
-
-print("model created")
+print("Model training complete and saved as chatbot_model.h5")
