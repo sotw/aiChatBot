5f8035af9fce2f034635b5ca11b4036574a6385a
KK.T <workthethird@gmail.com>
2026-03-04 08:01:21 +0800

    Update SOP4_save_gensim_format.py

--- prepareMLangVec/SOP4_save_gensim_format.py
+++ prepareMLangVec/SOP4_save_gensim_format.py
@@ -1,10 +1,13 @@
-from gensim.models.fasttext import load_facebook_vectors
+from gensim.models import KeyedVectors
 
-# 1. Load the slow way one last time
-print("Loading FB binary... (this will take a few minutes)")
-wv = load_facebook_vectors('cjk_english_300.bin')
+# 1. Load the Word2Vec binary file (Corrected from SOP3 output)
+print("Loading Word2Vec binary format...")
+# This will be MUCH faster and won't throw unicode errors
+wv = KeyedVectors.load_word2vec_format('cjk_english_300.bin', binary=True)
 
-# 2. Save it in Gensim's high-speed native format
-# This will create multiple files (e.g., .kv, .kv.vectors_ngrams.npy)
+# 2. Save it in Gensim's native format for mmap loading
+print("Saving as native Gensim KeyedVectors...")
 wv.save('cjk_fasttext.kv')
-print("Saved as native Gensim KeyedVectors!")
+
+print("Success! You can now load it instantly with:")
+print("ft_kv = KeyedVectors.load('cjk_fasttext.kv', mmap='r')")
